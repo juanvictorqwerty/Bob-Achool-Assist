de3257c35c6aab133de2e8ed609ad917cfdea7a0
MIKE Juan Victor <juanvictorqwerty@gmail.com>
2026-01-27 22:15:18 +0100

    connect to db

---
+++ server/config/db.js
@@ -0,0 +1,26 @@
+import mysql2 from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config(); //adds the env
+
+const pool=mysql2.createPool({
+    host : process.env.DB_Host,
+    user : process.env.DB_User,
+    password: process.env.DB_Password,
+    database: process.env.DB_Name,
+    connectionLimit:100,
+    queueLimit:0,
+    waitForConnections:true
+})
+
+const checkConnection= async()=>{
+    try{
+        const connection=await pool.getConnection();
+        console.log("Database connected")
+        connection.release();
+    }catch(error){
+        console.log("Connection failed")
+        throw error
+    }
+}
+export {pool,checkConnection}
